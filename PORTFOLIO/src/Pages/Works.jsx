0994833379726d76d1835b0aa083119a21394913
Shoaib Khan <shoaib7878687@gmail.com>
2026-05-04 14:52:03 +0500

    updated changes

--- PORTFOLIO/src/Pages/Works.jsx
+++ PORTFOLIO/src/Pages/Works.jsx
@@ -1,10 +1,209 @@
 import React from 'react'
 import './Works.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faShareFromSquare } from "@fortawesome/free-regular-svg-icons";
 
 const Works = () => {
   return (
-    <div>
-      Works
+    <div className='Works'>
+      <div className='text4'>
+      <h1>My <span>Projects</span></h1>
+      </div>
+      <div className='Projects'>
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project2.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Age Calculator</h1>
+        <p>Developed an Age Calculator using JavaScript that calculates and displays the user’s age based on their date of birth, focusing on accuracy and a simple user-friendly interface.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://age-calculator-two-orcin.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Age-Calculator" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project3.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Currency Converter</h1>
+        <p>Developed a Currency Converter (Currency Counter) using JavaScript that allows users to convert values between different currencies. The project focuses on real-time calculation, user-friendly interface, and practical real-world usage.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://currency-converter-with-react-eight.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Currency-Converter-With-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+        
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project4.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Count Character</h1>
+        <p>Developed a Character Counter application using JavaScript that counts the number of characters in a given input. This project focuses on real-time input handling and improving user interaction with simple and efficient functionality.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://count-character-and-sentence.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Count-Character-and-Sentence" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+        
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+        <div className='project'>
+          <div className='img'>
+        <img src="./src/Images/project1.png" width="160px" height="150px" alt="" />
+        </div>
+        <h1>Landing Page</h1>
+        <p>Developed a modern and responsive landing page using React, focusing on clean UI, smooth user experience, and optimized performance. This project highlights my ability to build real-world frontend applications with a professional design approach.</p>
+        <button>HTML</button>
+        <button style={{marginLeft:"10px"}}>CSS</button>
+        <button style={{marginLeft:"10px"}}>JavaScript</button>
+        <button style={{marginLeft:"10px"}}>React</button>
+        <div className='links'>
+        <button><a href="https://landing-page-form-react.vercel.app/" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faShareFromSquare} /> Live</a></button>
+        <button style={{marginLeft:"10px"}}><a  href="https://github.com/Shoaib-Khan90/Landing-Page-Form-React" target='_blank'><FontAwesomeIcon style={{fontSize:"20px"}} icon={faGithub} size="2x" /> Github</a></button>
+        </div>
+        </div>
+
+      </div>
     </div>
   )
 }
